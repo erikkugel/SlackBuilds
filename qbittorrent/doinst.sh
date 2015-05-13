35dc26189982f45822d18af46aa69fa8c545b3d5
erikkugel <erik.kugel@gmail.com>
2015-05-12 23:28:08 -0400

    Fixed merge junk

--- qbittorrent/doinst.sh
+++ qbittorrent/doinst.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ( if [ "$(getconf LONG_BIT)" == "64" ]; then
         LIBDIRSUFFIX="64"
 else
@@ -12,14 +11,3 @@ fi )
 	mkdir -v -p /var/run/qbittorrent
 fi 
 chown -c -R qbittorrent:qbittorrent /var/run/qbittorrent )
-=======
-( if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
-fi )
-
-( if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
-  if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
-  fi
-fi )
->>>>>>> qbittorrent
