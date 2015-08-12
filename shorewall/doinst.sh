7b6eaf4dee0a88e3e6f4806e6e8bfd87be4ef1d3
erikkugel <erik.kugel@gmail.com>
2015-08-11 21:18:37 -0400

    Use a single find command for all new configs

--- shorewall/doinst.sh
+++ shorewall/doinst.sh
@@ -1,10 +1,6 @@
-for NEW_CONFIG_FILE in $(find /etc/shorewall -type f); do
+for NEW_CONFIG_FILE in $(find /etc/shorewall /etc/logrotate.d/shorewall.new -type f -print); do
   CONFIG_FILE=$(basename ${NEW_CONFIG_FILE} .new)
   if ! [ -f ${CONFIG_FILE} ]; then
     mv ${NEW_CONFIG_FILE} ${CONFIG_FILE}
   fi
 done
-
-if [ -f /etc/logrotate.d/shorewall.new ] && [ ! -f /etc/logrotate.d/shorewall ]; then
-  mv -v -f /etc/logrotate.d/shorewall.new /etc/logrotate.d/shorewall
-fi
