f31c90fc09fcbb668ef9abf99bb16177e6321e99
erikkugel <erik.kugel@gmail.com>
2015-08-11 21:24:41 -0400

    Fix greedy find

--- shorewall/doinst.sh
+++ shorewall/doinst.sh
@@ -1,4 +1,4 @@
-for NEW_CONFIG_FILE in $(find /etc/shorewall /etc/logrotate.d/shorewall.new -type f -print); do
+for NEW_CONFIG_FILE in $(find /etc/shorewall/*.new /etc/logrotate.d/shorewall.new -type f -print); do
   CONFIG_FILE=$(basename ${NEW_CONFIG_FILE} .new)
   if ! [ -f ${CONFIG_FILE} ]; then
     mv ${NEW_CONFIG_FILE} ${CONFIG_FILE}
