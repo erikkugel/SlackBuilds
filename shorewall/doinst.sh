676450cacdfc55d3121afbc4c932c4a0a6f4166a
erikkugel <erik.kugel@gmail.com>
2015-08-13 18:58:24 -0400

    fixed broken config file management

--- shorewall/doinst.sh
+++ shorewall/doinst.sh
@@ -1,6 +1,6 @@
 for NEW_CONFIG_FILE in $(find /etc/shorewall/*.new /etc/logrotate.d/shorewall.new -type f -print); do
-  CONFIG_FILE=$(basename ${NEW_CONFIG_FILE} .new)
+  CONFIG_FILE=$(dirname ${NEW_CONFIG_FILE})/$(basename ${NEW_CONFIG_FILE} .new)
   if ! [ -f ${CONFIG_FILE} ]; then
-    mv ${NEW_CONFIG_FILE} ${CONFIG_FILE}
+    mv -v ${NEW_CONFIG_FILE} ${CONFIG_FILE}
   fi
 done
