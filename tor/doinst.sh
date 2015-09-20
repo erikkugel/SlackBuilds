f0d15d6b4e3693ac602f94c2aa2923daba8d0973
erikkugel <erik.kugel@gmail.com>
2015-09-19 22:47:50 -0400

    Fixed rc and moved everything to /usr/local

--- tor/doinst.sh
+++ tor/doinst.sh
@@ -1,13 +1,24 @@
+# Link any dynamic libraries
 ( if [ "$(getconf LONG_BIT)" == "64" ]; then
         LIBDIRSUFFIX="64"
 else
         LIBDIRSUFFIX=""
 fi
 /sbin/ldconfig -v -n /usr/local/lib${LIBDIRSUFFIX} )
+# Create a user
 ( if [ -z "$(getent passwd tor)" ]; then
-	/usr/sbin/useradd tor -m -U -G audio,video,cdrom -c "TOR"
+	/usr/sbin/useradd -r -U -c "TOR" -d / -s /bin/false tor
 fi )
-( if [ ! -d /var/run/tor ]; then
-	mkdir -v -p /var/run/tor
+# Create any required directories
+( if [ ! -d /usr/local/var/lib/tor/keys ]; then
+	mkdir -v -p /usr/local/var/lib/tor/keys
 fi 
-chown -c -R tor:tor /var/run/tor )
+chown -c -R tor:tor /usr/local/var/lib/tor )
+( if [ ! -d /usr/local/var/log/tor ]; then
+	mkdir -v -p /usr/local/var/log/tor
+fi 
+chown -c -R tor:tor /usr/local/var/log/tor )
+( if [ ! -d /usr/local/var/lib/tor ]; then
+	mkdir -v -p /usr/local/var/lib/tor 
+fi 
+chown -c -R tor:tor /usr/local/var/lib/tor )
