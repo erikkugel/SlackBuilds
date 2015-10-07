e0ebb591ebfea13f29094c163e418e8e36c5e342
erikkugel <erik.kugel@gmail.com>
2015-10-06 20:22:54 -0400

    Tor init

--- tor/doinst.sh
+++ tor/doinst.sh
@@ -1,13 +1,6 @@
-# Link any dynamic libraries
-( if [ "$(getconf LONG_BIT)" == "64" ]; then
-        LIBDIRSUFFIX="64"
-else
-        LIBDIRSUFFIX=""
-fi
-/sbin/ldconfig -v -n /usr/local/lib${LIBDIRSUFFIX} )
 # Create a user
 ( if [ -z "$(getent passwd tor)" ]; then
-	/usr/sbin/useradd -r -U -c "TOR" -d / -s /bin/false tor
+	/usr/sbin/useradd -r -m -U -c "Tor" tor
 fi )
 # Create any required directories
 ( if [ ! -d /usr/local/var/lib/tor/keys ]; then
