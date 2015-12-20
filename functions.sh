36ae3b987085fdd62935d174187326f921da1941
erikkugel <erik.kugel@gmail.com>
2015-12-20 14:27:29 -0500

    Refactor shared code into shared functions, add tag support for git

---
+++ functions.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#
+# Functions for SlackBuilds
+
+function git-pull {
+	name=$1
+	url=$2
+	tag=$3
+
+	# Clone/Pull latest
+	if [ -d ./${name}/.git ]; then
+	        cd ./${name}
+		git checkout master
+	        git pull
+	        cd ..
+	else
+	        rm -rfv ./${name}
+	        git clone $url $name
+	fi
+	# Use a Git tag if specified
+	cd ./${name}
+	if [ ${tag} ]; then
+		git checkout tags/${tag}
+	fi
+	cd ..
+}
+
+function stage {
+	source_dir=$1
+        build_dir=$2
+
+	mkdir -v -p ${build_dir}/install ${build_dir}/src
+	cp -v -f ./slack-desc ${build_dir}/install/slack-desc
+        if [ -f ./doinst.sh ]; then
+		cp -v -f ./doinst.sh ${build_dir}/install/doinst.sh
+	fi
+	cp -aRpfv ${source_dir} ${build_dir}/src
+}
+
+
+function build {
+	name=$1
+	version=$2
+	tag=$3
+	build_dir=$4
+	configure_opts=$5
+	build_opts=$6
+
+	# Compile
+	cd ${build_dir}/src/${name}
+	chown -Rc root:root .
+	./bootstrap.sh
+
+	if [ "$(getconf LONG_BIT)" == "64" ]; then
+	        lib_dir_suffix="64"
+	else
+	        lib_dir_suffix=""
+	fi	
+	./configure \
+		--prefix=/usr/local \
+		--libdir=/usr/local/lib${lib_dir_suffix} ${configure_opts}
+
+	make install \
+		-j$(getconf _NPROCESSORS_ONLN) \
+		DESTDIR=${build_dir} \
+		V=99
+	cd ${build_dir} 
+	rm -v -r -f ${build_dir}/src
+	# Build package
+	/sbin/makepkg -l y ${build_opts} -c n \
+		/tmp/${name}-${version}-$(uname -m)-${tag}.txz
+	rm -v -r -f ${build_dir}
+}
