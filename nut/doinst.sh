376f2ffe6c539460deaad452e11f324e93c55104
erikkugel <erik.kugel@gmail.com>
2017-12-13 23:57:11 -0500

    Fixed postinstall folders and permissions

--- nut/doinst.sh
+++ nut/doinst.sh
@@ -3,10 +3,15 @@
 else
         LIBDIRSUFFIX=""
 fi
-/sbin/ldconfig -v -n /usr/local/lib${LIBDIRSUFFIX} )
+/sbin/ldconfig -v -n /usr/lib${LIBDIRSUFFIX} )
 ( if [ -z "$(getent group nut)" ]; then
 	/usr/sbin/groupadd nut
 fi
 if [ -z "$(getent passwd ups)" ]; then
 	/usr/sbin/useradd ups -g nut -c "UPS"
+fi )
+( if ! [ -d /var/state/ups ]; then
+	mkdir -v -p /var/state/ups
+	chown ups:nut /var/state/ups
+	chmod og-rwx /var/state/ups
 fi )
