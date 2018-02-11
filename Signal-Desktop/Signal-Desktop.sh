c12db124b2c5cca1731aa44a01a872a474683d31
erikkugel <erik.kugel@gmail.com>
2018-02-11 17:15:02 -0500

    Signal Desktop build cleanup

--- Signal-Desktop/Signal-Desktop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /opt/Signal-Desktop && NODE_ENV=production yarn run start 2>&1>/dev/null &
--- Signal-Desktop/doinst.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /opt/Signal-Desktop \
-	&& npm install -g yarn \
-	&& yarn install
