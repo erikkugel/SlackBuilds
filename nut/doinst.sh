cdd27e9eaa477283505f5ec505d321e627b3c3e5
Ernest Kugel <erik@laptop1.lan>
2019-02-17 21:26:32 -0500

    improvements to nut ups

--- nut/doinst.sh
+++ nut/doinst.sh
@@ -15,3 +15,18 @@ fi )
 	chown ups:nut /var/state/ups
 	chmod og-rwx /var/state/ups
 fi )
+# Suggested by `makepkg`
+( cd usr/lib64 ; rm -rf libnutclient.so.0 )
+( cd usr/lib64 ; ln -sf libnutclient.so.0.0.0 libnutclient.so.0 )
+( cd usr/lib64 ; rm -rf libnutscan.so )
+( cd usr/lib64 ; ln -sf libnutscan.so.1.0.0 libnutscan.so )
+( cd usr/lib64 ; rm -rf libnutclient.so )
+( cd usr/lib64 ; ln -sf libnutclient.so.0.0.0 libnutclient.so )
+( cd usr/lib64 ; rm -rf libupsclient.so )
+( cd usr/lib64 ; ln -sf libupsclient.so.4.0.0 libupsclient.so )
+( cd usr/lib64 ; rm -rf libupsclient.so.4 )
+( cd usr/lib64 ; ln -sf libupsclient.so.4.0.0 libupsclient.so.4 )
+( cd usr/lib64 ; rm -rf libnutscan.so.1 )
+( cd usr/lib64 ; ln -sf libnutscan.so.1.0.0 libnutscan.so.1 )
+# Trigger udev after installing NUT UPS rules
+/sbin/udevadm control --reload-rules && /sbin/udevadm trigger
