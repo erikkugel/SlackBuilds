2823c7981073a825623bbe7e17f470a3da7db30e
Ernest Kugel <erik@laptop1.lan>
2019-07-28 15:37:55 -0400

    Updated to 5.2, fixed configuration files update on upgrade

--- shorewall/doinst.sh
+++ shorewall/doinst.sh
@@ -1,6 +1,6 @@
 for NEW_CONFIG_FILE in $(find /etc/shorewall/*.new /etc/logrotate.d/shorewall.new -type f -print); do
   CONFIG_FILE=$(dirname ${NEW_CONFIG_FILE})/$(basename ${NEW_CONFIG_FILE} .new)
-  if ! [ -f ${CONFIG_FILE} ]; then
+  if [ ! -f ${CONFIG_FILE} ] || [ -z "$(diff ${NEW_CONFIG_FILE} ${CONFIG_FILE})" ]; then
     mv -v ${NEW_CONFIG_FILE} ${CONFIG_FILE}
   fi
 done
