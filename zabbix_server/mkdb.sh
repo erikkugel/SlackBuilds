537a7f2ea67355be2812ae2a185172799a5444c5
Ernest Kugel <erik@laptop1.lan>
2020-05-17 15:04:32 -0400

    Fixed missing install script and bugs in database creation script

--- zabbix_server/mkdb.sh
+++ zabbix_server/mkdb.sh
@@ -1,16 +1,17 @@
-set -e
+set -e -x
 
 echo 'Checking for database, provide root password: '
-if [ -z "$(mysql -uroot -p -e 'SHOW DATABASES LIKE "zabbix";' -E)" ]; then
+read -s -p 'MySQL root password: ' MYSQL_PASSWD
+if [ -z "$(mysql -uroot -p${MYSQL_PASSWD} -e 'SHOW DATABASES LIKE "zabbix";' -E)" ]; then
         echo 'Configuring database...'
-        read -s -p 'New Zabbix database password: ' DB_PASSWD
-        echo 'Populating database, provide Zabbix database password: '
-        mysql -uroot -p -e "create database zabbix character set utf8 collate utf8_bin;
-create user 'zabbix'@'localhost' identified by \"${DB_PASSWD}\";
+        read -s -p 'New Zabbix database password: ' ZABBIX_DB_PASSWD
+        mysql -u root -p${MYSQL_PASSWD} -e "create database zabbix character set utf8 collate utf8_bin;
+create user 'zabbix'@'localhost' identified by '${ZABBIX_DB_PASSWD}';
 grant all privileges on zabbix.* to 'zabbix'@'localhost';"
-        mysql zabbix -uzabbix -p"${DB_PASSWD}" < /usr/share/zabbix/databasescripts/mysql/schema.sql
-        mysql zabbix -uzabbix -p"${DB_PASSWD}" < /usr/share/zabbix/databasescripts/mysql/images.sql
-        mysql zabbix -uzabbix -p"${DB_PASSWD}" < /usr/share/zabbix/databasescripts/mysql/data.sql
+        echo 'Populating database, provide Zabbix database password: '
+        mysql zabbix -u zabbix -p${ZABBIX_DB_PASSWD} < /usr/share/zabbix/databasescripts/mysql/schema.sql
+        mysql zabbix -u zabbix -p${ZABBIX_DB_PASSWD} < /usr/share/zabbix/databasescripts/mysql/images.sql
+        mysql zabbix -u zabbix -p${ZABBIX_DB_PASSWD} < /usr/share/zabbix/databasescripts/mysql/data.sql
 else
-        echo "Database exists, skipping"
+        echo 'Database exists, skipping.'
 fi
