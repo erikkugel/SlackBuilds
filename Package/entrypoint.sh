6f7c223c98c14a15c3c26743fb2532d27ce42f38
Ernest Kugel <erik.kugel@gmail.com>
2022-08-21 22:09:45 -0400

    Add Dockerfile and entrypoint

---
+++ Package/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo -------DEBUG-------
+echo
+echo "UName: $(uname -a)"
+echo -e "Dirs:\n$(ls -d /SlackBuilds)\n$(ls -d /packages)"
+echo "TARGETS: ${TARGETS}"
+echo "ARGS: $@"
+echo -e "Is GitHub Action? ${GITHUB_ACTIONS}"
+echo
+echo -----END DEBUG-----
+
+if [ "${GITHUB_ACTIONS}" == 'true' ]; then
+	echo "Use GitHub Actions mounts..."
+	SLACKBUILDS_DIRECTORY=/github/workspace
+	PACKAGES_DIRECTORY=/github/workspace/packages
+else
+	echo "Use Docker volumes under /SlackBuild and /packages..."
+	SLACKBUILDS_DIRECTORY=/SlackBuilds
+	PACKAGES_DIRECTORY=/packages
+fi
+
+if [ -n ${1} ] && [ -z ${TARGETS} ]; then
+	TARGETS=${1}
+fi
+
+IFS=','
+for TARGET in ${TARGETS}; do
+	echo "Building ${TARGET}..."
+	cd ${SLACKBUILDS_DIRECTORY}/${TARGET}
+	./${TARGET}.SlackBuild 2>&1 | tail -n 10
+	installpkg /tmp/${TARGET}-*.txz
+	cp /tmp/${TARGET}-*.txz ${PACKAGES_DIRECTORY}
+	echo "${TARGET} Done."
+done
+
