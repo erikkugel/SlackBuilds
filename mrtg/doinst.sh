c192d2f06b6afcaa46f9d962b053ceee091c5ed6
Ernest Kugel <erik.kugel@gmail.com>
2022-10-13 21:52:57 -0400

    Added MRTG

---
+++ mrtg/doinst.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Create user
+if [ -z "$(getent passwd mrtg)" ]; then
+	/usr/sbin/useradd -r -U -c 'MRTG' mrtg
+fi
+
+chown mrtg:mrtg /var/run/mrtg /var/www/htdocs/mrtg
+
+if [ -f /etc/mrtg.cfg ]; then
+	CFG_OUTPUT=/etc/mrtg.cfg.new
+else
+	CFG_OUTPUT=/etc/mrtg.cfg
+fi
+/usr/bin/cfgmaker \
+	--global 'WorkDir: /var/www/htdocs/mrtg' \
+	--global 'Options[_]: bits,growright' \
+	--global 'RunAsDaemon: Yes' \
+	--ifref=ip \
+	--output ${CFG_OUTPUT} \
+	"public@${HOSTNAME}"
