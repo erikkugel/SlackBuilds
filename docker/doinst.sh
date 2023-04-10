8be83479decdf1ccdf6bf97b15f223a6b39fa2f4
Ernest Kugel <erik.kugel@gmail.com>
2023-04-09 21:26:19 -0400

    Startup script for Docker Engine

--- docker/doinst.sh
+++ docker/doinst.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-if [ -z "$(grep docker /etc/group)" ]; then
+if [ -z "$(getent group docker)" ]; then
 	/usr/sbin/groupadd -r docker
 fi
+
+if [ -z "$(getent passwd docker)" ]; then
+	/usr/sbin/useradd -g docker -r docker
+fi
+
